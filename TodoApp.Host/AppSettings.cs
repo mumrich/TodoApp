8512abf9332f1adb92efca0b0b123175f60ef284
Hannes Morgenthaler <hmo@ingtes.ch>
2023-08-23 12:02:14 +0200

    working with manual tweak (node not beeing started).

---
+++ TodoApp.Host/AppSettings.cs
@@ -0,0 +1,11 @@
+using Mumrich.SpaDevMiddleware.Domain.Contracts;
+using Mumrich.SpaDevMiddleware.Domain.Models;
+
+namespace TodoApp.Host;
+
+public class AppSettings : ISpaDevServerSettings
+{
+    public Dictionary<string, SpaSettings> SinglePageApps { get; set; } = new();
+    public string SpaRootPath { get; set; } = Directory.GetCurrentDirectory();
+    public bool UseParentObserverServiceOnWindows { get; set; } = true;
+}
--- TodoApp.Host/Program.cs
+++ TodoApp.Host/Program.cs
@@ -1,6 +1,33 @@
+using Mumrich.SpaDevMiddleware.Domain.Types;
+using Mumrich.SpaDevMiddleware.Extensions;
+using TodoApp.Host;
+
 var builder = WebApplication.CreateBuilder(args);
+var appSettings = new AppSettings();
+
+appSettings.SinglePageApps.Add("/", new()
+{
+    DevServerAddress = "http://localhost:3456",
+    SpaRootPath = "todo-app",
+    NodeStartScript = "dev",
+    NodeBuildOutputPath = "build",
+    NodePackageManager = NodePackageManager.Yarn,
+    Regex = "dev server running at:",
+    Bundler = BundlerType.ViteJs,
+    SpaAssetsExpression = "^(src|node_modules|favicon.+|@[a-zA-Z]+|.*vite.*|.*\\.json|.*\\.js|.*\\.css)$"
+});
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddCors();
+builder.RegisterSinglePageAppDevMiddleware(appSettings);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors();
+app.MapControllers();
+app.MapSinglePageApps(appSettings);
 
 app.Run();
