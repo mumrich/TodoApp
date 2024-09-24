3a495f25efacd331acfe629d130a7101edca583a
Hannes Morgenthaler <hanneszbinden@outlook.com>
2024-09-24 13:45:17 +0200

    wip

--- TodoApp.Host/AppSettings.cs
+++ TodoApp.Host/AppSettings.cs
@@ -7,5 +7,4 @@ public class AppSettings : ISpaDevServerSettings
 {
     public Dictionary<string, SpaSettings> SinglePageApps { get; set; } = new();
     public string SpaRootPath { get; set; } = Directory.GetCurrentDirectory();
-    public bool UseParentObserverServiceOnWindows { get; set; } = false;
 }
